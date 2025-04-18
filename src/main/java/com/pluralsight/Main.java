129d4d1fde33a2bd8928de3739a9e3beefcbb5d0
paulo cunha <paulofranklinc@gmail.com>
2025-04-17 20:31:36 -0700

    updated my to have awhile running screen, need to refactor and clean up the code.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -8,7 +8,20 @@ import com.pluralsight.sentenceanalysis.StringAnalyzer;
 
 import java.util.Scanner;
 
+import static com.pluralsight.reusable.ReusableCode.inputForInt;
+
 public class Main {
+    static int userChoice;
+
+    static void mainScreen(Scanner scanner) {
+        System.out.println("1 - Calculator");
+        System.out.println("2 - String Analyzer");
+        System.out.println("3 - Tip Calculator");
+        System.out.println("4 - Temperature Converter");
+        System.out.println("5 - Date Formatter");
+        userChoice = inputForInt(scanner, "Choice: ");
+    }
+
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
         var stringAnalyzer = new StringAnalyzer();
@@ -17,16 +30,37 @@ public class Main {
         var temperatureConverter = new TemperatureConverter();
         var dateFormatter = new DateFormatter();
 
-        //Calculator
-//        stringAnalyzer.run(scanner);
-        //String Analyzer
-//        calculator.run(scanner);
-        //Tip Calculator
-//        tipCalculator.run(scanner);
-        //Temperature Converter
-//        temperatureConverter.run(scanner);
-        //Date Formatter
-        dateFormatter.run(scanner);
+        var running = true;
+        while (running) {
+            mainScreen(scanner);
+            scanner.nextLine();
+            switch (userChoice) {
+                case 1:
+                    //Calculator
+                    calculator.run(scanner);
+                    break;
+                case 2:
+                    //String Analyzer
+                    stringAnalyzer.run(scanner);
+                    break;
+                case 3:
+                    //Tip Calculator
+                    tipCalculator.run(scanner);
+                    break;
+                case 4:
+                    //Temperature Converter
+                    temperatureConverter.run(scanner);
+                    break;
+                case 5:
+                    //Date Formatter
+                    dateFormatter.run(scanner);
+                    break;
+                case 0:
+                    running = false;
+                default:
+                    mainScreen(scanner);
+            }
+        }
         scanner.close();
     }
 }
