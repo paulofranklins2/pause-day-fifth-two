fc67c334187a3a0fcf4daf687e3c8614412bf0b2
paulofranklins <paulofranklinc@gmail.com>
2025-04-21 12:00:51 -0700

    room/house exercise

---
+++ src/main/java/com/pluralsight/constructors/Room.java
@@ -0,0 +1,13 @@
+package com.pluralsight.constructors;
+
+public class Room {
+    private String name;
+
+    Room(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
