97e39cec70565059848347be1c4cc772331da66d
paulofranklins <paulofranklinc@gmail.com>
2025-04-21 12:00:37 -0700

    planet exercise

---
+++ src/main/java/com/pluralsight/constructors/Planet.java
@@ -0,0 +1,12 @@
+package com.pluralsight.constructors;
+
+public class Planet {
+    private final String NAME;
+    private final int SIZE;
+
+    Planet(String name, int size) {
+        this.NAME = name;
+        this.SIZE = size;
+    }
+
+}
