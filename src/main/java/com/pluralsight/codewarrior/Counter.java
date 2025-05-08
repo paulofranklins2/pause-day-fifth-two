733c963afe17d437711677edc553e87b02218a76
paulofranklins <paulofranklinc@gmail.com>
2025-05-08 11:44:58 -0700

    new codewar commit

--- src/main/java/com/pluralsight/codewarrior/Counter.java
+++ src/main/java/com/pluralsight/codewarrior/Counter.java
@@ -2,13 +2,13 @@ package com.pluralsight.codewarrior;
 
 public class Counter {
     public static void main(String[] args) {
-        System.out.println(countSheeps(new Boolean[]{
+        System.out.println(countSheep(new Boolean[]{
                 true, true, true, false, true, true, true, null, true, true, false, true, false,
                 true, false, false, true, true, true, true, true, false, false, true, true}
         ));
     }
 
-    public static int countSheeps(Boolean[] arrayOfSheeps) {
+    public static int countSheep(Boolean[] arrayOfSheeps) {
         var counter = 0;
         for (int i = 0; i < arrayOfSheeps.length; i++) {
             if (arrayOfSheeps[i] != null && arrayOfSheeps[i]) {
